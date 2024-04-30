371575a844c9a93b8db5da6e77d37c8c4a2709dd
한소영 <fjw_1010@naver.com>
2024-04-30 19:03:15 +0900

    movie.js

---
+++ src/movie.js
@@ -0,0 +1,42 @@
+export const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NTBlOWYzZWM4MDE2MDNjNDU0ZGE2OTI3YzE2NzgyOCIsInN1YiI6IjY2MjlmY2Y2NTBmN2NhMDBiNGM4NzRkMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZL5HraHPiqZGsswyXAE56sifz70OMVx3b1YwgO__5B8",
+  },
+};
+
+fetch(
+  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => {
+    const movieList = response.results;
+    movieList.forEach((element) => {
+      console.log(element);
+
+      const movieCard = document.createElement("div");
+      movieCard.className = "boxlist";
+      movieCard.innerHTML = `
+        <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2${element.poster_path}" alt="영화 포스터" />
+        <h1 class="movieTitle">${element.title}</h1>
+        <p>${element.overview}</p>
+        <p>Rating : ${element.popularity}</p>
+        `;
+      document.getElementById("movieList").appendChild(movieCard);
+    });
+
+    movieList.addEventListener("click", handleClickCard);
+    function handleClickCard(event) {
+      // 카드 영역 외 영역 클릭 무시
+      if (event.target === movieList) return;
+      if (event.target.matches("boxlist")) {
+        alert(`영화 id: ${event.target.id}`);
+      } else {
+        alert(`영화 id: ${event.target.parentNode.id}`);
+      }
+    }
+  })
+  .catch((err) => console.error(err));
