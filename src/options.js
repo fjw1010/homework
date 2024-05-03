f0347e1bbdfcf1ce61d917711e77ec0f49226c9a
한소영 <fjw_1010@naver.com>
2024-05-03 12:11:12 +0900

    api 사이트 정보 값

---
+++ src/options.js
@@ -0,0 +1,8 @@
+export const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NTBlOWYzZWM4MDE2MDNjNDU0ZGE2OTI3YzE2NzgyOCIsInN1YiI6IjY2MjlmY2Y2NTBmN2NhMDBiNGM4NzRkMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZL5HraHPiqZGsswyXAE56sifz70OMVx3b1YwgO__5B8",
+  },
+};
