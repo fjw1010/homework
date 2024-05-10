649c4b17a5faae48ea0421e2870dd12cac8b1620
한소영 <fjw_1010@naver.com>
2024-05-10 18:08:39 +0900

    영화 페이지
    tmdb api 주소를 받아서 연결 시킨 뒤, 홈페이지에 20개의 영화 정보 리스트를 나타나게 하고 싶었으나 아직 건들지 못 함.
    추후에 건들일 예정...

--- src/movie.js
+++ src/movie.js
@@ -11,7 +11,7 @@ fetch(
       console.log(element);
 
       const movieCard = document.createElement("div");
-      movieCard.idName = "box";
+      movieCard.idName = movie.id;
       movieCard.className = "boxlist";
       movieCard.innerHTML = `
         <img class="moviePoster" src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2${element.poster_path}" alt="영화 포스터"" />
@@ -20,17 +20,7 @@ fetch(
         <p class="movieRating">Rating : ${element.popularity}</p>
         `;
       document.getElementById("movieList").appendChild(movieCard);
+      document.addEventListener("click", handleClickCard);
     });
-
-    movieList.addEventListener("click", handleClickCard);
-    function handleClickCard(event) {
-      // 카드 영역 외 영역 클릭 무시
-      if (event.target === movieList) return;
-      if (event.target.matches("boxlist")) {
-        alert(`영화 id: ${event.target.id}`);
-      } else {
-        alert(`영화 id: ${event.target.parentNode.id}`);
-      }
-    }
   })
   .catch((err) => console.error(err));
